916024ea6a12d6325df580a0ec716a9a9e83e076
Khushboo Dalal <khushboodalal1434@gmail.com>
2023-06-30 21:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 //your JS code here. If required.
+/*const line = document.getElementById('line');
 
-    #line {
-      width: 200px;
-      height: 2px;
-      background-color: black;
-      animation: rotateLine 2s infinite linear;
-		position: absolute;
-    }
+let angle = 0;
 
-    @keyframes rotateLine {
-      0% {
-        transform: rotate(0deg);
-      }
-      100% {
-        transform: rotate(360deg);
-      }
-    }
-  
+function rotateLine() {
+
+  angle++;
+
+  line.style.transform = `rotate(${angle}deg)`;
+
+  requestAnimationFrame(rotateLine);
+}
+
+rotateLine();*/
